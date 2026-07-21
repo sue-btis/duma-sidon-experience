7e722c7edfe68f5f2b9639491b5dfd86c4957211
sue_btis <josuee152@gmail.com>
2026-07-21 11:11:36 -0600

    feat(home): update WorldCircuit SVG with mask for circuit line gaps and adjust flow dot animations

--- apps/web/src/features/home/WorldCircuit.tsx
+++ apps/web/src/features/home/WorldCircuit.tsx
@@ -16,8 +16,20 @@ export function WorldCircuit({ close, lead, title }: Props) {
       <p className={styles.worldsLead}>{lead}</p>
       <div className={styles.worldCircuit}>
         <svg aria-hidden="true" className={styles.circuitLines} preserveAspectRatio="none" viewBox="0 0 1000 1000">
-          <path d="M395 440 C310 440 290 180 167 180 M605 440 C690 440 710 180 833 180 M500 590 L500 790" />
-          <path d="M167 250 V720 Q167 860 307 860 H360 M833 250 V720 Q833 860 693 860 H640" />
+          <defs>
+            <mask id="circuitLineGaps" maskUnits="userSpaceOnUse">
+              <rect fill="white" height="1000" width="1000" />
+              <circle cx="167" cy="180" fill="black" r="110" />
+              <circle cx="500" cy="440" fill="black" r="130" />
+              <circle cx="833" cy="180" fill="black" r="110" />
+              <circle cx="200" cy="823" fill="black" r="24" />
+              <circle cx="800" cy="823" fill="black" r="24" />
+            </mask>
+          </defs>
+          <g mask="url(#circuitLineGaps)">
+            <path d="M395 440 C310 440 290 180 167 180 M605 440 C690 440 710 180 833 180 M500 590 L500 790" />
+            <path d="M167 250 V720 Q167 860 307 860 H360 M833 250 V720 Q833 860 693 860 H640" />
+          </g>
           <circle className={styles.flowDot} r="7">
             <animateMotion dur="8s" path="M395 440 C310 440 290 180 167 180" repeatCount="indefinite" />
           </circle>
@@ -28,7 +40,7 @@ export function WorldCircuit({ close, lead, title }: Props) {
             <animateMotion begin="-2s" dur="8s" path="M167 250 V720 Q167 860 307 860 H360" repeatCount="indefinite" />
           </circle>
           <circle className={styles.flowDot} r="7">
-            <animateMotion begin="-6s" dur="8s" path="M833 250 V720 Q833 860 693 860 H640" repeatCount="indefinite" />
+            <animateMotion begin="-2s" dur="8s" path="M833 250 V720 Q833 860 693 860 H640" repeatCount="indefinite" />
           </circle>
           <circle className={styles.flowDot} r="7">
             <animateMotion begin="-1s" dur="8s" path="M500 860 L500 590" repeatCount="indefinite" />
